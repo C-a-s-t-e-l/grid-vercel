1609f2aafe8ac22133d814b804792b49fcd715ca
CasCodes <ezzyllena@gmail.com>
2025-07-19 10:08:28 +0800

    updated seo

--- api/sitemap.js
+++ api/sitemap.js
@@ -1,7 +1,5 @@
-// api/sitemap.js (Corrected Version)
 const { createClient } = require('@supabase/supabase-js');
 
-// It's secure to expose the anon key here
 const supabaseUrl = 'https://nslmlulajnbinbzurhrz.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5zbG1sdWxham5iaW5ienVyaHJ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE5NDQzMjcsImV4cCI6MjA2NzUyMDMyN30.M7bF4Hw_dUP8tS7MPJf-sAZQaPBZ7Ar5hf2gde_w1EU';
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
